49876e97c170fe3897f01df03166eb51ec468b80
hanyoungYoo <hanyoung0411@handong.edu>
2021-08-20 16:14:38 +0900

    Fix build script to set local source emplicitly

--- packages/build.sh
+++ packages/build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-[ ! -e $(basename $0) ] && exit 1
+set -x
+
+[ -e $(basename $0) ] && exit 1
 
 PROJECT_LIST=("gpac" "clamav" "libjpeg-turbo")
 OSSFUZZ_DIR=$(dirname $(dirname `pwd`))/oss-fuzz
@@ -78,7 +80,7 @@ case $BUILD_MODE in
 	    python3 $INFRA_DIR/build_specified_commit.py --project_name $PROJECT --commit $FIX_COMMIT  --engine $FUZZER --sanitizer $SANITIZER
 	    ;;
     "single")
-	    python3 $INFRA_DIR/helper.py build_fuzzers --sanitizer $SANITIZER $PROJECT `pwd`/$PROJECT-repo 
+	    python3 $INFRA_DIR/helper.py build_fuzzers --clean --sanitizer $SANITIZER $PROJECT --mount_path `pwd`/$PROJECT-repo 
 	    ;;
     *)
 	    echo "INVALID BUILD MODE"
